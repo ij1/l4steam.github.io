108ac92067c8755755bc52255b604e6965ab7666
Ilpo Järvinen <ilpo.jarvinen@cs.helsinki.fi>
2021-06-03 17:52:09 +0300

    Make coalescing AccECN aware

--- ack-coalescing/ack_coalescer/strategy.c
+++ ack-coalescing/ack_coalescer/strategy.c
@@ -1,9 +1,15 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/time.h>
 
+#include <arpa/inet.h>
+#include <netinet/tcp.h>
+
 #include "coalescer.h"
+
+int accecn_aware = 1;
 
 static struct event event_q = { .fl = NULL, .next = NULL };
 
@@ -76,6 +82,80 @@ void free_packets(struct flow *fl)
 	fl->pkt_count = 0;
 }
 
+#define TCPOPT_EXP              254     /* Experimental */
+/* Magic number to be after the option value for sharing TCP
+ * experimental options. See draft-ietf-tcpm-experimental-options-00.txt
+ */
+#define TCPOPT_ACCECN0_MAGIC    0xACC0
+#define TCPOPT_ACCECN1_MAGIC    0xACC1
+
+
+bool packet_has_accecn_option(struct packet *pkt)
+{
+	int optspace = (pkt->tcp->th_off << 2) - sizeof(struct tcphdr);
+	unsigned char *opt = (unsigned char *)(pkt->tcp) + sizeof(struct tcphdr);
+	uint16_t magic;
+
+	while (optspace > 0) {
+		switch (*opt) {
+		case TCPOPT_EOL:
+			return false;
+		case TCPOPT_NOP:
+			opt++;
+			optspace--;
+			break;
+		case TCPOPT_EXP:
+			if (optspace < 4)
+				return false;
+			magic = ntohs(*((uint16_t *)&opt[2]));
+			if (magic == TCPOPT_ACCECN0_MAGIC ||
+			    magic == TCPOPT_ACCECN1_MAGIC) {
+				if (optspace - opt[1] < 0)
+					return false;
+				return true;
+			}
+
+		default:
+			if (optspace < 2)
+				return false;
+			optspace -= opt[1];
+			opt += opt[1];
+			break;
+		}
+	}
+	return false;
+}
+
+void accecn_aware_send(struct flow *fl)
+{
+	struct packet *pkt;
+	struct packet *sendlist[3];
+	unsigned int sendcnt = 0;
+	unsigned int accecncnt = 0;
+	int i;
+
+	if (fl->pkt_count <= 0)
+		return;
+
+	pkt = fl->pkt;
+	sendlist[sendcnt++] = pkt;
+
+	while (pkt != NULL) {
+		if (packet_has_accecn_option(pkt)) {
+			if (pkt != fl->pkt)
+				sendlist[sendcnt++] = pkt;
+
+			accecncnt++;
+			if (accecncnt >= 2)
+				break;
+		}
+		pkt = pkt->next;
+	}
+
+	for (i = sendcnt - 1; i >= 0; i--)
+		send_packet(sendlist[i]);
+}
+
 void immediate(struct flow *fl, bool timeout)
 {
 	send_packet(fl->pkt);
@@ -87,7 +167,12 @@ void halfdrop(struct flow *fl, bool timeout)
 	packet_count++;
 	if (packet_count > init_period_packets && fl->pkt_count < 2)
 		return;
-	send_packet(fl->pkt);
+
+	if (accecn_aware) {
+		accecn_aware_send(fl);
+	} else {
+		send_packet(fl->pkt);
+	}
 	free_packets(fl);
 }
 
@@ -96,7 +181,12 @@ void every16(struct flow *fl, bool timeout)
 	packet_count++;
 	if (packet_count > init_period_packets && fl->pkt_count < 16)
 		return;
-	send_packet(fl->pkt);
+
+	if (accecn_aware) {
+		accecn_aware_send(fl);
+	} else {
+		send_packet(fl->pkt);
+	}
 	free_packets(fl);
 }
 
@@ -106,7 +196,11 @@ void ackreqgrant(struct flow *fl, bool timeout)
 {
 	if (timeout) {
 		del_event_for_flow(fl);
-		send_packet(fl->pkt);
+		if (accecn_aware) {
+			accecn_aware_send(fl);
+		} else {
+			send_packet(fl->pkt);
+		}
 		free_packets(fl);
 
 		fl->timeout.tv_usec = 0;
